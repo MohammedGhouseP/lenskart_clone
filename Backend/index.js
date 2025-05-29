6976668f4d94bfae2cdcefe3c83c4a11fa0e1970
MohammedGhouseP <ghousebashamail@gmail.com>
2025-05-29 19:34:36 +0530

    bug resolved

--- Backend/index.js
+++ Backend/index.js
@@ -26,10 +26,10 @@ app.use("/cart", cartRouter);
 app.listen(process.env.port, async () => {
   try {
     await connection;
-    console.log("Connected to the MongoDB");
+    console.log("Connected to the DB");
   } catch (err) {
-    console.log("Trouble connecting to the DB");
+    console.log("Trouble connection to DB");
     console.log(err);
   }
-  console.log(`Running at ${process.env.port} Port`);
+  console.log(`Running at ${process.env.PORT} Port`);
 });
